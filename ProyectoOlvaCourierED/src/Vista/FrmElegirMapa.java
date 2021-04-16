2dbad02bfb272b91508cdcc8a978679735082d64
AlexL <alexlizarazo20@gmail.com>
2021-04-15 22:18:19 -0500

    pendiente 2

--- ProyectoOlvaCourierED/src/Vista/FrmElegirMapa.java
+++ ProyectoOlvaCourierED/src/Vista/FrmElegirMapa.java
@@ -251,7 +251,7 @@ public class FrmElegirMapa extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(fondo, javax.swing.GroupLayout.DEFAULT_SIZE, 1055, Short.MAX_VALUE)
+                .addComponent(fondo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
